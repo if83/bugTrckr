a1de092d8e2f33bba92127646dc0e974b01cd4b7
prokhorenkovkv <prokhorenkov.kv@gmail.com>
2016-08-05 13:12:10 +0300

    rename method

--- src/main/java/com/softserverinc/edu/controllers/WorkLogController.java
+++ src/main/java/com/softserverinc/edu/controllers/WorkLogController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/worklog")
 public class WorkLogController {
 
-    public String worklogIndex() {
+    public String Index() {
         return "worklog";
     }
 
