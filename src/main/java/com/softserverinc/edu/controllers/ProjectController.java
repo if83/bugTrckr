773d6d8172d308ea84937281c17245ed1ff7bb6d
prokhorenkovkv <prokhorenkov.kv@gmail.com>
2016-08-05 13:10:58 +0300

    rename method

--- src/main/java/com/softserverinc/edu/controllers/ProjectController.java
+++ src/main/java/com/softserverinc/edu/controllers/ProjectController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/project")
 public class ProjectController {
 
-    public String worklogIndex() {
+    public String Index() {
         return "project";
     }
 
