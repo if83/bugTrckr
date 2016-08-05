24f146c88e5ab0c45d4319970067d21a910bf469
prokhorenkovkv <prokhorenkov.kv@gmail.com>
2016-08-05 13:11:12 +0300

    rename method

--- src/main/java/com/softserverinc/edu/controllers/ReleaseController.java
+++ src/main/java/com/softserverinc/edu/controllers/ReleaseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/release")
 public class ReleaseController {
 
-    public String worklogIndex() {
+    public String Index() {
         return "release";
     }
 
